50c80b63ef82fa91d3272be509840eb0d6151ff7
Isack Munguia <isack.munguia.mejia@gmail.com>
2018-11-22 03:24:17 -0600

    se agrega entidad clases aprobadas

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pensumsRouter = require('./routes/pensumsRouter');
 var encuestasRouter = require('./routes/encuestasRouter');
+var clasesAprobadasRouter = require('./routes/clasesAprobadasRouter');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pensums', pensumsRouter);
 app.use('/encuestas', encuestasRouter);
+app.use('/caprobadas', clasesAprobadasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
